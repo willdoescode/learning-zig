dcc097c71e9b3b57a78c88ea292075e59525bf24
Will Lane <williamlane923@gmail.com>
2021-03-18 00:56:44 -0700

    sorting list

--- src/main.zig
+++ src/main.zig
@@ -1067,3 +1067,11 @@ test "stack" {
         }));
     }
 }
+
+test "sorting" {
+    var data = [_]u8{ 10, 240, 0, 0, 10, 5 };
+    std.sort.sort(u8, &data, {}, comptime std.sort.asc(u8));
+    expect(eql(u8, &data, &[_]u8{ 0, 0, 5, 10, 10, 240 }));
+    std.sort.sort(u8, &data, {}, comptime std.sort.desc(u8));
+    expect(eql(u8, &data, &[_]u8{ 240, 10, 10, 5, 0, 0 }));
+}
