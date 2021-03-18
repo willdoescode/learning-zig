7ac9488e1cb29fedad8e8dccf11289882eb031d1
Will Lane <williamlane923@gmail.com>
2021-03-18 09:54:41 -0700

    custom iterator

--- src/main.zig
+++ src/main.zig
@@ -1115,3 +1115,31 @@ test "arg iteration" {
 
     expect(arg_chars > 0);
 }
+
+const ContainsIterator = struct {
+    strings: []const []const u8,
+    needle: []const u8,
+    index: usize = 0,
+
+    fn next(self: *ContainsIterator) ?[]const u8 {
+        const index = self.index;
+        for (self.strings[index..]) |string| {
+            self.index += 1;
+            if (std.mem.indexOf(u8, string, self.needle)) |_| {
+                return string;
+            }
+        }
+        return null;
+    }
+};
+
+test "custom iterator" {
+    var iter = ContainsIterator{
+        .strings = &[_][]const u8{ "one", "two", "three" },
+        .needle = "e",
+    };
+
+    expect(eql(u8, iter.next().?, "one"));
+    expect(eql(u8, iter.next().?, "three"));
+    expect(iter.next() == null);   
+}
