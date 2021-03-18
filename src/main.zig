82362eb5c603c024a479d73a569ade15fb95c837
Will Lane <williamlane923@gmail.com>
2021-03-18 00:53:45 -0700

    stack

--- src/main.zig
+++ src/main.zig
@@ -1037,3 +1037,33 @@ test "string hashmap" {
     expect(map.get("me").? == .cool);
     expect(map.get("loris").? == .uncool);
 }
+
+test "stack" {
+    const string = "(()())";
+
+    var stack = std.ArrayList(usize).init(test_allocator);
+    defer stack.deinit();
+
+    const Pair = struct { open: usize, close: usize };
+
+    var pairs = std.ArrayList(Pair).init(test_allocator);
+    defer pairs.deinit();
+
+    for (string) |char, i| {
+        if (char == '(') try stack.append(i);
+        if (char == ')')
+            try pairs.append(.{
+                .open = stack.pop(),
+                .close = i,
+        });
+    }
+
+    for (pairs.items) |pair, i| {
+        expect(std.meta.eql(pair, switch (i) {
+            0 => Pair{ .open = 1, .close = 2 },
+            1 => Pair{ .open = 3, .close = 4 },
+            2 => Pair{ .open = 0, .close = 5 },
+            else => unreachable,
+        }));
+    }
+}
