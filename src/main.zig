9a84a53af36aa67cf1015f921b18c46a491b545b
Will Lane <williamlane923@gmail.com>
2021-03-17 23:33:46 -0700

    custom fmt

--- src/main.zig
+++ src/main.zig
@@ -842,3 +842,61 @@ test "array printing" {
         "{ 1, 4 } + { 2, 5 } = { 3, 9 }",
     ));
 }
+
+const Person = struct {
+    name: []const u8,
+    birth_year: i32,
+    death_year: ?i32,
+    
+    pub fn format(
+        self: Person,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype
+    ) !void {
+        try writer.print("{s} ({}-", .{self.name, self.birth_year});
+
+        if (self.death_year) |year| try writer.print("{}", .{year});
+        try writer.writeAll(")");
+    }
+};
+
+test "custom fmt" {
+    const john = Person{
+        .name = "John Carmack",
+        .birth_year = 1970,
+        .death_year = null,
+    };
+
+    const john_string = try std.fmt.allocPrint(
+        test_allocator,
+        "{s}",
+        .{john},
+    );
+    defer test_allocator.free(john_string);
+
+    expect(eql(
+        u8,
+        john_string,
+        "John Carmack (1970-)",
+    ));
+
+    const claude = Person{
+        .name = "Claude Shannon",
+        .birth_year = 1916,
+        .death_year = 2001,
+    };
+
+    const claude_string = try std.fmt.allocPrint(
+        test_allocator,
+        "{s}",
+        .{claude},
+    );
+    defer test_allocator.free(claude_string);
+
+    expect(eql(
+        u8,
+        claude_string,
+        "Claude Shannon (1916-2001)",
+    ));
+}
